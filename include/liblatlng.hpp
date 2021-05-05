67c0f43ef2b573d946ec5219b514a8ca291ebd1a
masaka <masaka@mz-kb.com>
2021-05-06 00:50:09 +0900

    Add liblatlng.hpp

---
+++ include/liblatlng.hpp
@@ -0,0 +1,170 @@
+#pragma once
+#include <cmath> // std::isnan
+
+#if __cplusplus >= 202002L
+    #include <numbers> // std::numbers::pi_v
+#endif
+
+namespace liblatlng
+{
+
+    #if __cplusplus >= 202002L
+        // C++20以降なら定数πにnumbersヘッダを使用
+        namespace Constants
+        {
+            template <typename T>
+            constexpr T pi() { return std::numbers::pi_v<T>; }
+        }
+    #else
+        // C++20より前なら定数πを手動で指定
+        // (_USE_MATH_DEFINESによるマクロ汚染を避けるためM_PIは不使用)
+        namespace Constants
+        {
+            template <typename T>
+            constexpr T pi() { return 3.14159265358979323846; }
+        }
+    #endif
+
+    namespace Degree
+    {
+        // 角度からラジアンへ
+        template <typename T>
+        constexpr T toRadian(T deg)
+        {
+            return deg * Constants::pi<T>() / T{180.0};
+        }
+
+        // ラジアンから角度へ
+        template <typename T>
+        constexpr T fromRadian(T rad)
+        {
+            return rad * T{180.0} / Constants::pi<T>();
+        }
+
+        // 角度を -180 ～ 180 の範囲に丸める
+        template <typename T>
+        T normalizeRelative(T deg)
+        {
+            // NaNは足しても引いてもNaNのままなので無限ループに陥る
+            if (std::isnan(deg))
+            {
+                return deg;
+            }
+
+            // 値の指数が大きいと360を足しても引いても変わらなくて無限ループに陥る
+            if (deg > 1e9 || deg < -1e9)
+            {
+                return 0.0;
+            }
+
+            while (deg >= 180.0)
+            {
+                deg -= 360.0;
+            }
+
+            while (deg < -180.0)
+            {
+                deg += 360.0;
+            }
+
+            return deg;
+        }
+
+        // 角度を 0 ～ 360 の範囲に丸める
+        template <typename T>
+        T normalizeAbsolute(T deg)
+        {
+            // NaNは足しても引いてもNaNのままなので無限ループに陥る
+            if (std::isnan(deg))
+            {
+                return deg;
+            }
+
+            // 値の指数が大きいと360を足しても引いても変わらなくて無限ループに陥る
+            if (deg > 1e9 || deg < -1e9)
+            {
+                return T{0.0};
+            }
+
+            while (deg >= 360.0)
+            {
+                deg -= 360.0;
+            }
+
+            while (deg < 0.0)
+            {
+                deg += 360.0;
+            }
+
+            return deg;
+        }
+    }
+
+    // 緯度/経度(クラステンプレート)
+    template <typename T>
+    struct BasicLatLng
+    {
+        // 緯度(latitude), 単位:度
+        T lat;
+
+        // 経度(longitude), 単位:度
+        T lng;
+
+        constexpr BasicLatLng(T lat, T lng)
+            : lat(lat)
+            , lng(lng)
+        {
+        }
+
+        // 指定地点からの距離を返す(メートル単位)
+        T distanceFrom(const BasicLatLng<T> & other) const;
+
+        // 指定地点から矢印を引いた場合の方位を返す(単位:度, 0～360の範囲)
+        T azimuthFrom(const BasicLatLng<T> & other) const;
+    };
+
+    template <typename T>
+    T BasicLatLng<T>::distanceFrom(const BasicLatLng<T> & other) const
+    {
+        // 地球の長半径
+        constexpr T kEarthRadius = 6378137.0;
+
+        // 度からラジアンへ変換
+        const T latRadian = Degree::toRadian(lat); // 緯度
+        const T lngRadian = Degree::toRadian(lng); // 経度
+        const T otherLatRadian = Degree::toRadian(other.lat);
+        const T otherLngRadian = Degree::toRadian(other.lng);
+
+        // 経度の差分
+        const T lngRadianDiff = otherLngRadian - lngRadian;
+
+        return kEarthRadius * std::acos(
+            std::sin(latRadian) * std::sin(otherLatRadian) +
+            std::cos(latRadian) * std::cos(otherLatRadian) * std::cos(lngRadianDiff)
+        );
+    }
+
+    template <typename T>
+    T BasicLatLng<T>::azimuthFrom(const BasicLatLng<T> & other) const
+    {
+        // 度からラジアンへ変換
+        const T latRadian = Degree::toRadian(lat); // 緯度
+        const T lngRadian = Degree::toRadian(lng); // 経度
+        const T otherLatRadian = Degree::toRadian(other.lat);
+        const T otherLngRadian = Degree::toRadian(other.lng);
+
+        // 経度の差分
+        const T lngRadianDiff = otherLngRadian - lngRadian;
+
+        const T y = std::sin(lngRadianDiff);
+        const T x = std::cos(latRadian) * std::tan(otherLatRadian) - std::sin(latRadian) * std::cos(lngRadianDiff);
+        return Degree::normalizeAbsolute(Degree::fromRadian(std::atan2(y, x)) + 180.0);
+    }
+
+    // 緯度/経度(double型)
+    using LatLng = BasicLatLng<double>;
+
+    // 緯度/経度(float型)
+    using LatLngF = BasicLatLng<float>;
+
+}
